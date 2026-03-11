7290b5c03594000aa40854935bf77fb309a72d4c
dhairyalakhmani <dhairyalakhmani27@gmail.com>
2026-03-11 15:25:13 +0530

    Replaced context functionality with zustand stores

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import PageNotFound from "components/commons/PageNotFound";
 import Product from "components/Product";
 import ProductList from "components/ProductList";
@@ -7,25 +5,20 @@ import { NavLink, Route, Switch, Redirect } from "react-router-dom";
 import routes from "routes";
 
 import "./App.css";
-import CartItemsContext from "./contexts/CartItemsContext";
 
-const App = () => {
-  const [cartItems, setCartItems] = useState([]);
-
-  return (
-    <CartItemsContext.Provider value={[cartItems, setCartItems]}>
-      <div className="flex space-x-2">
-        <NavLink exact activeClassName="underline font-bold" to="/" />
-        <NavLink exact activeClassName="underline font-bold" to="/product" />
-      </div>
-      <Switch>
-        <Route exact component={ProductList} path={routes.products.index} />
-        <Route exact component={Product} path={routes.products.show} />
-        <Redirect exact from={routes.root} to={routes.products.index} />
-        <Route component={PageNotFound} path="*" />
-      </Switch>
-    </CartItemsContext.Provider>
-  );
-};
+const App = () => (
+  <>
+    <div className="flex space-x-2">
+      <NavLink exact activeClassName="underline font-bold" to="/" />
+      <NavLink exact activeClassName="underline font-bold" to="/product" />
+    </div>
+    <Switch>
+      <Route exact component={ProductList} path={routes.products.index} />
+      <Route exact component={Product} path={routes.products.show} />
+      <Redirect exact from={routes.root} to={routes.products.index} />
+      <Route component={PageNotFound} path="*" />
+    </Switch>
+  </>
+);
 
 export default App;
